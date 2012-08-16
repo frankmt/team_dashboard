2fd2396b5b816bdee45da83c04fd8665ac2db179
Geoffrey Giesemann <geoffrey.giesemann@rea-group.com>
2012-08-16 11:55:55 +1000

    Add config for unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,22 @@
+base_path = File.expand_path('../../', __FILE__)
+
+worker_processes 2
+working_directory base_path + '/tmp'
+
+preload_app true
+timeout 60
+
+pid base_path + '/tmp/unicorn.pid'
+
+stderr_path base_path + '/log/unicorn.stderr.log'
+stderr_path base_path + '/log/unicorn.stdout.log'
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.clear_all_connections!
+end
+
+after_fork do |server, workder|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
